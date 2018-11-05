39bc37d3842a3972039020141c3b051e6c60a9a1
oike.takeshi <o2gpsh@gmail.com>
2018-11-05 23:14:04 +0900

    Lesson5 3Dゲームを作ろうの課題提出 20191105 <初回プッシュ>
    Lesson5の課題提出になります。
    得点表示の課題までの提出となり、発展課題の方はまだ手付かずです。まずはこちらで合格を貰ってから発展課題に進みたいと思います。
    よろしくお願いします。

---
+++ Assets/BallController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	//変数の宣言
+	int score = 0;
+
+	//ボールが見える可能性があるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバーを表示するテキスト
+	private GameObject gameoverText;
+	private GameObject scoreText;
+
+	// Use this for initialization
+	void Start () {
+
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+
+		//シーン中のScoreTextオブジェクトを取得
+		this.scoreText = GameObject.Find("ScoreText");
+
+	}
+
+	// 可算処理の実行
+	void OnCollisionEnter(Collision other) {
+
+		if (other.gameObject.tag == "SmallStarTag") {
+			// 加算の処理
+			score += 10;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			// 加算の処理
+			score += 30;
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			// 加算の処理
+			score += 20;
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			// 加算の処理
+			score += 50;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+
+			//GameoverTextにゲームオーバーを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over\r\n\r\nあなたの得点は" + score + "点です。";
+
+		//ボールが画面内にある場合
+		} else if (this.transform.position.z > this.visiblePosZ) {
+
+			//ScoreTextに得点を表示
+			this.scoreText.GetComponent<Text> ().text = "Score:" + score ;
+
+		}
+
+	}
+
+}
---
+++ Assets/CloudController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour {
+
+	//最小サイズ
+	private float minimum = 1.0f;
+	//拡大縮小スピード
+	private float magSpeed = 10.0f;
+	//拡大率
+	private float magnification = 0.07f;
+
+	//最小サイズ（サイズ変更）
+	//private float minimum = 2.0f;
+	//拡大縮小スピード（スピード変更）
+	//private float magSpeed = 20.0f;
+	//拡大率（拡大率変更）
+	//private float magnification = 0.20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//雲を拡大縮小
+		this.transform.localScale = new Vector3(this.minimum + Mathf.Sin(Time.time * this.magSpeed) * this.magnification, this.transform.localScale.y, this.minimum + Mathf.Sin(Time.time * this.magSpeed) * this.magnification);
+		
+	}
+}
